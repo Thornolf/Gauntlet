398a284e408d5fd873119ca46a983b5d85659810
Fossaert Thomas <thomas.fossaert@epitech.eu>
2017-05-09 09:49:03 +0200

    Test ogre

---
+++ ogre/CMakeFiles/OgreApp.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fossae_t/Téléchargements/clean_ogre_cmake_project_1-9(2)/clean_ogre_cmake_project/BaseApplication.cpp" "/home/fossae_t/Téléchargements/clean_ogre_cmake_project_1-9(2)/clean_ogre_cmake_project/CMakeFiles/OgreApp.dir/BaseApplication.cpp.o"
+  "/home/fossae_t/Téléchargements/clean_ogre_cmake_project_1-9(2)/clean_ogre_cmake_project/TutorialApplication.cpp" "/home/fossae_t/Téléchargements/clean_ogre_cmake_project_1-9(2)/clean_ogre_cmake_project/CMakeFiles/OgreApp.dir/TutorialApplication.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/OIS"
+  "/usr/local/include/OGRE"
+  "/usr/local/include"
+  "/usr/local/share/OGRE/samples/Common/include"
+  "/usr/local/include/OGRE/Overlay"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
