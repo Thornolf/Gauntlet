6f3e2bd577cb298b7b1d1b02b0fb7c6ad4858355
Guillaume CAUCHOIS <guillaume.cauchois@epitech.eu>
2017-05-22 16:05:53 +0200

    [GAUNTLET-19] Configuration
    Ajout de la class configuration
    Manque un pointeur vers les players

---
+++ include/Configuration.hpp
@@ -0,0 +1,29 @@
+/*
+** Configuration.hpp for IndieStudio in /home/guillobits/workspace/epitech/cpp_indie_studio/include/Configuration.hpp
+**
+** Made by Guillaume CAUCHOIS
+** Login   <guillaume.cauchois@epitech.eu>
+**
+** Started on  Mon May 22 13:07:27 2017 Guillaume CAUCHOIS
+** Last update Mon May 22 13:07:27 2017 Guillaume CAUCHOIS
+*/
+
+#ifndef		_CONFIGURATION__HPP_
+# define	_CONFIGURATION__HPP_
+
+/* # include	"InputManager.hpp" */
+/* # include	"PC.h" */
+
+class Configuration
+{
+public:
+  /* Canonical functions */
+  Configuration();
+  virtual ~Configuration();
+  Configuration		&operator=(const Configuration &);
+
+private:
+  /* std::vector<NPC> */
+};
+
+#endif		/* _CONFIGURATION__HPP_ */
