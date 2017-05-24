cc3944bd0719b8552e43d925d9405c63085565d7
Unknown <adrien.warin@epitech.eu>
2017-05-24 11:19:01 +0200

    [GAUNTLET-55] Ranged
    Terminé - MASTER

--- include/Ranged.hpp
+++ include/Ranged.hpp
@@ -5,5 +5,32 @@
 ** Login   <warin_a@epitech.net>
 **
 ** Started on  Wed May 24 10:15:37 2017 Adrien Warin
-** Last update Wed May 24 10:15:45 2017 Adrien Warin
+** Last update Wed May 24 11:06:15 2017 Adrien Warin
 */
+
+#ifndef RANGED_HPP_
+# define  RANGED_HPP_
+
+#include <iostream>
+#include "Pc.hpp"
+
+class Ranged : public Pc
+{
+    private:
+
+    public:
+        Ranged (int, int, int);
+        Ranged (Ranged const &);
+        Ranged& operator=(Ranged const &);
+        virtual ~Ranged ();
+
+        /* Méthodes */
+
+        virtual void getDamage();
+        virtual void attack();
+        virtual bool isAlive();
+        virtual void getDistance();
+        virtual size_t getRange();
+};
+
+#endif
