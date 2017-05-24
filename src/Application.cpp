ab80a0c2b4339674301ff444d11f5efd870b9e3f
Robin Grattepanche <robin.grattepanche@epitech.eu>
2017-05-24 16:06:06 +0200

    truc de base

--- src/Application.cpp
+++ src/Application.cpp
@@ -5,7 +5,7 @@
 ** Login   <fossae_t@epitech.net>
 **
 ** Started on  Fri May 19 15:02:47 2017 Thomas Fossaert
-** Last update Wed May 24 13:46:25 2017 Thomas Fossaert
+** Last update Wed May 24 16:05:45 2017 Robin Grattepanche
 */
 
 #include "Application.h"
@@ -23,17 +23,34 @@ void Application::createScene()
 {
   mSceneMgr->setAmbientLight(Ogre::ColourValue(.25, .25, .25));
 
+	mSceneMgr->setSkyDome(true, "Examples/CloudySky");
+
   Ogre::Light* pointLight = mSceneMgr->createLight("PointLight");
   pointLight->setType(Ogre::Light::LT_POINT);
   pointLight->setPosition(250, 150, 250);
-  pointLight->setDiffuseColour(Ogre::ColourValue::Green);
-  pointLight->setSpecularColour(Ogre::ColourValue::Green);
+  pointLight->setDiffuseColour(Ogre::ColourValue::White);
+  pointLight->setSpecularColour(Ogre::ColourValue::White);
 
-  Ogre::Entity* ninjaEntity = mSceneMgr->createEntity("penguin.mesh");
-  Ogre::SceneNode* ninjaNode = mSceneMgr->getRootSceneNode()->createChildSceneNode(
-    "NinjaNode");
-  ninjaNode->attachObject(ninjaEntity);
+  Ogre::Entity* penguinEntity = mSceneMgr->createEntity("penguin.mesh");
+  Ogre::SceneNode* pinguinNode = mSceneMgr->getRootSceneNode()->createChildSceneNode(
+    "PinguinNode");
+	pinguinNode->setPosition(0, 50, 0);
+  pinguinNode->attachObject(penguinEntity);
 
+	Ogre::Plane plane(Ogre::Vector3::UNIT_Y, 0);
+
+	Ogre::MeshManager::getSingleton().createPlane("ground",
+	Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+	plane, 1500, 1500, 20, 20, true, 1, 5, 5,
+	Ogre::Vector3::UNIT_Z);
+
+	// Ogre::SceneNode *cam = mSceneMgr->createChildSceneNode(Ogre::Vector3(0, 500, 350));
+	// cam->attachObject(mCamera);
+
+	Ogre::Entity* groundEntity = mSceneMgr->createEntity("ground");
+	mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(groundEntity);
+
+	groundEntity->setMaterialName("Examples/Rockwall");
 }
 
 bool Application::frameRenderingQueued(const Ogre::FrameEvent& fe)
@@ -50,8 +67,8 @@ bool Application::processUnbufferedInput(const Ogre::FrameEvent& fe)
 {
   static bool mouseDownLastFrame = false;
   static Ogre::Real toggleTimer = 0.0;
-  static Ogre::Real rotate = .13;
-  static Ogre::Real move = 50;
+  static Ogre::Real rotate = 0;
+  static Ogre::Real move = 100;
 
   // First toggle method
   bool leftMouseDown = mMouse->getMouseState().buttonDown(OIS::MB_Left);
@@ -79,36 +96,49 @@ bool Application::processUnbufferedInput(const Ogre::FrameEvent& fe)
   Ogre::Vector3 dirVec = Ogre::Vector3::ZERO;
 
   if (mKeyboard->isKeyDown(OIS::KC_K))
-    dirVec.x -= move;
+    {
+			rotate -= .3;
+			dirVec.x -= move;
+		}
 
   if (mKeyboard->isKeyDown(OIS::KC_M))
-    dirVec.x += move;
-
-  if (mKeyboard->isKeyDown(OIS::KC_O))
-    dirVec.z += move;
+		{
+			rotate += .3;
+    	dirVec.x += move;
+		}
 
   if (mKeyboard->isKeyDown(OIS::KC_L))
-    dirVec.z -= move;
+    dirVec.z += move;
 
-  if (mKeyboard->isKeyDown(OIS::KC_J))
-  {
-    if (mKeyboard->isKeyDown(OIS::KC_LSHIFT))
-      mSceneMgr->getSceneNode("NinjaNode")->yaw(Ogre::Degree(5 * rotate));
-    else
-      dirVec.x -= move;
-  }
+  if (mKeyboard->isKeyDown(OIS::KC_O))
+    {
+			// mSceneMgr->getSceneNode("PinguinNode")->yaw(Ogre::Degree(20 * rotate));
+			dirVec.z -= move;
+		}
 
-  if (mKeyboard->isKeyDown(OIS::KC_U))
-  {
-    if (mKeyboard->isKeyDown(OIS::KC_LSHIFT))
-      mSceneMgr->getSceneNode("NinjaNode")->yaw(Ogre::Degree(-5 * rotate));
-    else
-      dirVec.x += move;
-  }
+  // if (mKeyboard->isKeyDown(OIS::KC_J))
+  // {
+  //   if (mKeyboard->isKeyDown(OIS::KC_LSHIFT))
+  //     mSceneMgr->getSceneNode("PinguinNode")->yaw(Ogre::Degree(5 * rotate));
+  //   else
+  //     dirVec.x -= move;
+  // }
+	//
+  // if (mKeyboard->isKeyDown(OIS::KC_U))
+  // {
+  //   if (mKeyboard->isKeyDown(OIS::KC_LSHIFT))
+  //     mSceneMgr->getSceneNode("PinguinNode")->yaw(Ogre::Degree(-5 * rotate));
+  //   else
+  //     dirVec.x += move;
+  // }
 
-  mSceneMgr->getSceneNode("NinjaNode")->translate(
+  mSceneMgr->getSceneNode("PinguinNode")->translate(
     dirVec * fe.timeSinceLastFrame,
     Ogre::Node::TS_LOCAL);
 
+  mSceneMgr->getSceneNode("PinguinNode")->yaw(
+    Ogre::Degree(rotate * fe.timeSinceLastFrame));
+
+		rotate = 0;
   return true;
 }
