4d90be1c251b4218c931c09087f99d8f3b61bf64
Quentin Baudet <quentin.baudet@epitech.eu>
2017-05-31 10:07:58 +0200

    Update

--- src/GameObject/GameObject.cpp
+++ src/GameObject/GameObject.cpp
@@ -5,7 +5,7 @@
 ** Login   <fossae_t@epitech.net>
 **
 ** Started on  Fri May 19 17:05:43 2017 Thomas Fossaert
-** Last update Tue May 23 16:12:10 2017 Thomas Fossaert
+** Last update Wed May 31 09:53:00 2017 Quentin Baudet
 */
 
 #include "GameObject.hpp"
@@ -26,10 +26,7 @@ GameObject& GameObject::operator=(GameObject const & other)
   return *this;
 }
 
-GameObject::~GameObject()
-{
-
-}
+GameObject::~GameObject() {}
 
 Position* GameObject::getPosition() const
 {
